4c2268692a9605c5d975b422c937ed47a3864380
Orlando Avila <orlando.avilag@hotmail.com>
2023-09-15 23:17:02 -0600

    Selenium framework with Java, Cucumber and JUnit.

--- src/test/java/utils/ConstantData.java
+++ src/test/java/utils/ConstantData.java
@@ -4,8 +4,8 @@ import org.openqa.selenium.WebDriver;
 
 public class ConstantData {
     public static WebDriver chromeDriverInstance = null;
-    public static String chromeDriverPath = "./src/test/resources/drivers/chromedriver";
-    public static String geckoDriverPath = "./src/test/java/drivers/geckodriver";
+    public static String chromeDriverPath = "./src/test/resources/drivers/chromedriver.exe";
+    public static String geckoDriverPath = "./src/test/resources/drivers/geckodriver";
     public static String URL = "https://www.saucedemo.com";
     public static boolean chromeInstanceExists = false;
 }
